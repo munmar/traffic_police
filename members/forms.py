451e6a7bc6eadd1e8d8da1895beb0802e0a10657
Marton <marton.munkacsi09@gmail.com>
2023-10-09 02:58:13 +0100

    Changes:  - added user management  - custom user creation forms  - add/delete users

--- members/forms.py
+++ members/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -10,3 +10,13 @@ class PasswordChangingForm(PasswordChangeForm):
   class Meta:
     model = User
     fields = ('old_password', 'new_password1', 'new_password2')
+
+class CreateUserForm(UserCreationForm):
+  username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class': 'form-control'}))
+  password1 = forms.CharField(label="New Password", max_length=100, widget=forms.PasswordInput({'class': 'form-control', 'type': 'password'}))
+  password2 = forms.CharField(label="New Password", max_length=100, widget=forms.PasswordInput({'class': 'form-control', 'type': 'password'}))
+  is_superuser = forms.BooleanField(label="Admin Privileges", required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input', 'type': 'checkbox'}))
+
+  class Meta:
+    model = User
+    fields = ['username', 'password1', 'password2', 'is_superuser']
--- members/views.py
+++ members/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-# from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from django.contrib import messages
-from .forms import PasswordChangingForm
+from .forms import PasswordChangingForm, CreateUserForm
+from django.contrib.auth.models import User
+from django.db.models import Q
+
 
 # Create your views here.
 def login_user(request):
@@ -35,3 +38,40 @@ class PasswordsChangeView(PasswordChangeView):
   def form_valid(self, form):
     messages.success(self.request, "Your password has been changed successfully.")
     return super().form_valid(form)
+  
+def manage(request):
+  search_query = request.GET.get('search_query', '')
+  user_list = User.objects.filter(
+    Q(username__icontains=search_query) |
+    Q(password__icontains=search_query) |
+    Q(is_superuser__icontains=search_query)
+  )
+  current_page_name = 'Manage Users'
+  context = {
+    'users': user_list,
+    'current_page_name': current_page_name,
+  }
+  return render(request, 'management/manage.html', context)
+
+User = get_user_model()
+def add_user(request):
+  if request.method == 'POST':
+    form = CreateUserForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      user.is_superuser = form.cleaned_data['is_superuser']
+      user.save()
+      return redirect('manage')
+  else:
+    form = CreateUserForm()
+  
+  context = {
+    'form': form,
+  }
+  
+  return render(request, 'management/add_user.html', context)
+
+def delete_user(request, user_id):
+  user = get_object_or_404(User, id=user_id)
+  user.delete()
+  return redirect('manage')
