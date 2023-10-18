ef5b633e2cb1a978cb974b896328e5dd87f06526
Marton <marton.munkacsi09@gmail.com>
2023-10-18 16:40:56 +0100

    Changes:  - Added real object counts to cards

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from people.models import Person
+from vehicles.models import Vehicle
+from reports.models import Incident
 
-# Create your views here.
+# Home page view
 @login_required
 def dashboard(request):
   current_page_name = 'Home'
+  person_count = Person.objects.count()
+  vehicle_count = Vehicle.objects.count()
+  report_count = Incident.objects.count()
+
   context = {
-    'current_page_name': current_page_name
+    'current_page_name': current_page_name,
+    'person_count': person_count,
+    'vehicle_count': vehicle_count,
+    'report_count': report_count,
   }
   return render(request, 'dashboard/home.html', context)
 
+# Settings view
 @login_required
 def settings_view(request):
   current_page_name = 'Settings'
